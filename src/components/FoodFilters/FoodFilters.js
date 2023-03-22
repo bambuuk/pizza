464a5ffd9929b1b8511542941effd9eb31ca9dc5
Ivan <vanyok.korobka@ukr.net>
2023-03-22 15:59:32 +0200

    added select tag instead ul list in food filter to media rule max-width 767 px

--- src/components/FoodFilters/FoodFilters.js
+++ src/components/FoodFilters/FoodFilters.js
@@ -2,7 +2,7 @@ import './foodFilters.scss';
 
 const FoodFilters = () => {
   return (
-    <div className="food-filter">
+    <div className="food-filter food-filter_content">
       <ul className="food-filter__list">
         <li className="food-filter__item active-btn">ПІЦА</li>
         <li className="food-filter__item">БУРГЕР IS BACK</li>
@@ -11,6 +11,16 @@ const FoodFilters = () => {
         <li className="food-filter__item">ПИВО</li>
         <li className="food-filter__item">Напої / Cоуси</li>
       </ul>
+      <div className="food-filter_wrapper">
+        <select name="food-filter" className="drop-down-list">
+          <option className="drop-down-list__item">ПІЦА</option>
+          <option className="drop-down-list__item">БУРГЕР IS BACK</option>
+          <option className="drop-down-list__item">КРИЛА</option>
+          <option className="drop-down-list__item">ФРІТЮР</option>
+          <option className="drop-down-list__item">ПИВО</option>
+          <option className="drop-down-list__item">Напої / Cоуси</option>
+        </select>
+      </div>
     </div>
   )
 }
