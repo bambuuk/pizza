9f9b5afdf2142f7f844f0f7c16e6c8579d155ebb
Ivan <vanyok.korobka@ukr.net>
2023-04-03 16:07:06 +0300

    added form and layout to the order-form in FoodList component

--- src/components/FoodList/FoodList.js
+++ src/components/FoodList/FoodList.js
@@ -19,8 +19,8 @@ const FoodList = () => {
   const onActiveShopBag = (e) => {
     let clName = e.target.className;
     if (
-      clName === 'basket__img' || clName === 'order-list__close-btn' || 
-      clName === 'order-list_overlay' || 
+      clName === 'basket__img' || clName === 'order-list__close-btn' ||
+      clName === 'order-list_overlay' ||
       e.target.parentElement.className === 'order-list__close-btn'
     ) {
       setShoppingBag(shoppingBag => !shoppingBag);
@@ -96,36 +96,62 @@ const FoodList = () => {
                   <span></span>
                 </div>
               </div>
-              <div className="order-list__product-item">
-                <div className="order-list__img">
-                  <img src={img} alt="" />
-                </div>
-                <div className="order-list__descr">
-                  <div className="order-list__subtitle">SoloWay White SoloWay WhitE SoloWay WhitE SoloWay Whit SoloWay Whit</div>
-                  <div className="order-list__features">Розмір</div>
-                </div>
-                <div className="order-list__counter">
-                  <div className="order-list__minus order-list__sign">
-                    <span></span>
-                  </div>
-                  <span className="order-list__qty">1</span>
-                  <div className="order-list__plus order-list__sign">
-                    <span></span>
-                    <span></span>
-                  </div>
-                </div>
-                <div className="order-list__amount">585 грн</div>
-                <div className="order-list__del-product order-list__sign">
-                  <span></span>
-                  <span></span>
-                </div>
-              </div>
             </div>
             <div className="order-list__total-amount">Сума: 585 грн</div>
             <div className="order-list__form order-form">
               <form action="">
-                <label htmlFor=""></label>
-                <input type="text" />
+
+                <div className="order-form__enterInfo">
+                  <label className="order-form__usualLabel" htmlFor="name">Як до Вас звертатися:</label>
+                  <input className="order-form__usualInput" id="name" name="customer name" type="text" required />
+                </div>
+
+                <div className="order-form__enterInfo">
+                  <label className="order-form__usualLabel" htmlFor="phone-number">Номер телефону:</label>
+                  <input className="order-form__usualInput" id="phone-number" name="customer phone number" type="tel" required/>
+                </div>
+
+                <div className="order-form__enterInfo">
+                  <div className="order-form__subtitle">Варіанти доставки:</div>
+                  <p className="order-form__radio">
+                    <input type="radio" name="typeOfDelivery" id="deliveryAroundCity" value="Доставка по місту до 8 км 50 гривень"/>
+                    <label for="deliveryAroundCity">Доставка по місту до 8 км 50 гривень</label>
+                  </p>
+                  <p className="order-form__radio">
+                    <input type="radio" name="typeOfDelivery" id="pickup" value="Самовивіз за адресою вул. Пашутинська 61/84"/>
+                    <label for="pickup">Самовивіз за адресою вул. Пашутинська 61/84</label>
+                  </p>
+                </div>
+
+                <div className="order-form__enterInfo">
+                  <label className="order-form__usualLabel" htmlFor="address">Адреса</label>
+                  <input className="order-form__usualInput" id="address" name="delivery address" type="text" required />
+                </div>
+
+                <div className="order-form__enterInfo">
+                  <label className="order-form__usualLabel" htmlFor="comment">Коментар</label>
+                  <input className="order-form__usualInput" id="comment" name="customer comment" type="text" required />
+                </div>
+
+                <div className="order-form__enterInfo">
+                  <div className="order-form__subtitle">Спосіб оплати:</div>
+                  <p className="order-form__radio">
+                    <input type="radio" name="typeOfPayment" id="cash" value="Готівкою при отриманні"/>
+                    <label for="cash">Готівкою при отриманні</label>
+                  </p>
+                  <p className="order-form__radio">
+                    <input type="radio" name="typeOfPayment" id="online" value="Онлайн карткою Visa або Mastercard"/>
+                    <label for="online">Онлайн карткою Visa або Mastercard</label>
+                  </p>
+                </div>
+                
+                <div className="order-form__orderAmount">
+                  <div className="order-form__amount">Сума: 640грн</div>
+                  <div className="order-form__deliveryAmount">Доставка по місту до 8 км 50 гривень: 50грн</div>
+                  <div className="order-form__totalAmount">Загальна сума: 690грн</div>
+                </div>
+
+                <button className="order-form__orderBtn">Замовити</button>
               </form>
             </div>
           </div>
