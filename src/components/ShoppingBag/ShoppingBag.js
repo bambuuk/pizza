3d6a7d161465042a7bf705589711fe101117efb5
Ivan <vanyok.korobka@ukr.net>
2023-04-15 15:55:32 +0300

    made controlled form in ShoppingBag component

--- src/components/ShoppingBag/ShoppingBag.js
+++ src/components/ShoppingBag/ShoppingBag.js
@@ -1,8 +1,16 @@
+import { useState } from 'react';
 import { CSSTransition, TransitionGroup } from 'react-transition-group';
 
 import './shoppingBag.scss';
 
 const ShoppingBag = (props) => {
+  const [customerName, setCustomerName] = useState('');
+  const [customerPhoneNumber, setCustomerPhoneNumber] = useState('');
+  const [customerAddress, setCustomerAddress] = useState('');
+  const [customerComment, setCustomerCommet] = useState('');
+  const [typeOfDelivery, setTypeOfDelivery] = useState('');
+  const [typeOfPayment, setTypeOfPayment] = useState('');
+
   const {
     activeStyleShopBag,
     onActiveShopBag,
@@ -42,6 +50,8 @@ const ShoppingBag = (props) => {
     );
   }) : null;
 
+  const totalOrderSum = typeOfDelivery === 'deliveryAroundCity' ? (+orderSum + 50) : orderSum;
+
   return (
     <div className={`order-list ${activeStyleShopBag}`}>
       <div className="order-list_overlay" onClick={onActiveShopBag} >
@@ -64,52 +74,108 @@ const ShoppingBag = (props) => {
 
               <div className="order-form__enterInfo">
                 <label className="order-form__usualLabel" htmlFor="name">Як до Вас звертатися:</label>
-                <input className="order-form__usualInput" id="name" name="customer name" type="text" required />
+                <input 
+                  className="order-form__usualInput" 
+                  id="name" 
+                  name="customer name" 
+                  type="text"
+                  value={customerName}
+                  onChange={(e) => setCustomerName(e.target.value)} 
+                  required />
               </div>
 
               <div className="order-form__enterInfo">
                 <label className="order-form__usualLabel" htmlFor="phone-number">Номер телефону:</label>
-                <input className="order-form__usualInput" id="phone-number" name="customer phone number" type="tel" required />
+                <input 
+                  className="order-form__usualInput" 
+                  id="phone-number" 
+                  name="customer phone number" 
+                  type="tel" 
+                  value={customerPhoneNumber}
+                  onChange={(e) => setCustomerPhoneNumber(e.target.value)}
+                  required />
               </div>
 
               <div className="order-form__enterInfo">
                 <div className="order-form__subtitle">Варіанти доставки:</div>
                 <p className="order-form__radio">
-                  <input type="radio" name="typeOfDelivery" id="deliveryAroundCity" value="Доставка по місту до 8 км 50 гривень" />
+                  <input 
+                    type="radio" 
+                    name="typeOfDelivery" 
+                    id="deliveryAroundCity" 
+                    value={typeOfDelivery}
+                    onChange={() => setTypeOfDelivery('deliveryAroundCity')} 
+                  />
                   <label htmlFor="deliveryAroundCity">Доставка по місту до 8 км 50 гривень</label>
                 </p>
                 <p className="order-form__radio">
-                  <input type="radio" name="typeOfDelivery" id="pickup" value="Самовивіз за адресою вул. Пашутинська 61/84" />
+                  <input 
+                    type="radio" 
+                    name="typeOfDelivery" 
+                    id="pickup" 
+                    value={typeOfDelivery}
+                    onChange={() => setTypeOfDelivery('pickup')}  
+                  />
                   <label htmlFor="pickup">Самовивіз за адресою вул. Пашутинська 61/84</label>
                 </p>
               </div>
 
               <div className="order-form__enterInfo">
                 <label className="order-form__usualLabel" htmlFor="address">Адреса</label>
-                <input className="order-form__usualInput" id="address" name="delivery address" type="text" required />
+                <input 
+                  className="order-form__usualInput" 
+                  id="address" 
+                  name="delivery address" 
+                  type="text" 
+                  value={customerAddress}
+                  onChange={(e) => setCustomerAddress(e.target.value)}
+                  required />
               </div>
 
               <div className="order-form__enterInfo">
                 <label className="order-form__usualLabel" htmlFor="comment">Коментар</label>
-                <input className="order-form__usualInput" id="comment" name="customer comment" type="text" required />
+                <input 
+                  className="order-form__usualInput" 
+                  id="comment" 
+                  name="customer comment" 
+                  type="text"
+                  value={customerComment}
+                  onChange={(e) => setCustomerCommet(e.target.value)} 
+                />
               </div>
 
               <div className="order-form__enterInfo">
                 <div className="order-form__subtitle">Спосіб оплати:</div>
                 <p className="order-form__radio">
-                  <input type="radio" name="typeOfPayment" id="cash" value="Готівкою при отриманні" />
+                  <input 
+                    type="radio" 
+                    name="typeOfPayment" 
+                    id="cash" 
+                    value={typeOfPayment}
+                    onChange={() => setTypeOfPayment('cash')} 
+                  />
                   <label htmlFor="cash">Готівкою при отриманні</label>
                 </p>
                 <p className="order-form__radio">
-                  <input type="radio" name="typeOfPayment" id="online" value="Онлайн карткою Visa або Mastercard" />
+                  <input 
+                    type="radio" 
+                    name="typeOfPayment" 
+                    id="online" 
+                    value={typeOfPayment}
+                    onChange={() => setTypeOfPayment('online')}  
+                  />
                   <label htmlFor="online">Онлайн карткою Visa або Mastercard</label>
                 </p>
               </div>
 
               <div className="order-form__orderAmount">
                 <div className="order-form__amount">Сума: {orderSum} грн</div>
-                <div className="order-form__deliveryAmount">Доставка по місту до 8 км 50 гривень: 50грн</div>
-                <div className="order-form__totalAmount">Загальна сума: {orderSum} грн</div>
+                {
+                  typeOfDelivery === 'deliveryAroundCity' ?
+                  <div className="order-form__deliveryAmount">Доставка по місту до 8 км 50 гривень: 50грн</div> :
+                  null 
+                }
+                <div className="order-form__totalAmount">Загальна сума: {totalOrderSum} грн</div>
               </div>
 
               <button className="order-form__orderBtn">Замовити</button>
