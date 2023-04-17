767e1e252e9a09c21b3f85268d2376f563dea2b1
Ivan <vanyok.korobka@ukr.net>
2023-04-17 21:43:49 +0300

    added success window to the UserCabinet component and upd method showing user name and email

--- src/components/UserCabinet/UserCabinet.js
+++ src/components/UserCabinet/UserCabinet.js
@@ -9,10 +9,11 @@ import './userCabinet.scss';
 const cookies = new Cookies();
 
 const UserCabinet = (props) => {
+  const { activeLogRegWindow, toggleLogRegWindActive, setIsAuth } = props;
   const [currentUser, setCurrentUser] = useState({});
   const [currentUserName, setCurrentUserName] = useState('');
   const [listOrders, setListOrders] = useState([]);
-  const { activeLogRegWindow, toggleLogRegWindActive, setIsAuth } = props;
+  const [successWindow, setSuccessWindow] = useState(false);
 
   const ordersRef = collection(firestoreDb, 'orders');
 
@@ -25,15 +26,17 @@ const UserCabinet = (props) => {
 
   // adding correct user name to the state
   useEffect(() => {
-    if (currentUser.displayName) {
-      setCurrentUserName(currentUser.displayName);
-    } else if (Array.isArray(cookies.get('userNamesList'))) {
-      const arrOfUserNames = cookies.get('userNamesList');
-      arrOfUserNames.forEach(item => {
-        if (item.email === currentUser.email) {
-          setCurrentUserName(item.name);
-        }
-      })
+    if (currentUser) {
+      if (currentUser.displayName) {
+        setCurrentUserName(currentUser.displayName);
+      } else if (Array.isArray(cookies.get('userNamesList'))) {
+        const arrOfUserNames = cookies.get('userNamesList');
+        arrOfUserNames.forEach(item => {
+          if (item.email === currentUser.email) {
+            setCurrentUserName(item.name);
+          }
+        })
+      }
     }
   }, [currentUser]);
 
@@ -42,43 +45,50 @@ const UserCabinet = (props) => {
     try {
       await signOut(auth);
       localStorage.removeItem('auth-token-pizza');
-      setIsAuth(false);
-      toggleLogRegWindActive(null, 'logout');
+
+      if (successWindow === false) {
+        setSuccessWindow(true);
+
+        setTimeout(() => {
+          setIsAuth(false);
+          toggleLogRegWindActive(null, 'logout');
+          setSuccessWindow(false);
+        }, 3000);
+      }
       console.log('logout')
     } catch (err) {
-      console.log(err, 'error');
+      console.log(err.messaage, 'error');
     }
   }
 
   // getting info about ordered history
   useEffect(() => {
-    if (Boolean(currentUser.email) === true) {
-      // gets all messages with room name from props
-      const queryOrders = query(
-        ordersRef,
-        where("email", "==", currentUser.email),
-        orderBy('createdAt') // need to connect with using method query and add index in firestore 
-      );
+    if (currentUser) {
+      if (Boolean(currentUser.email) === true) {
+        // gets all messages with room name from props
+        const queryOrders = query(
+          ordersRef,
+          where("email", "==", currentUser.email),
+          orderBy('createdAt') // need to connect with using method query and add index in firestore 
+        );
 
-      // listening all changes for queryOrders
-      const unsuscribe = onSnapshot(queryOrders, (snapshot) => {
-        let newOrders = [];
-        snapshot.forEach((doc) => {
-          // console.log(doc, 'doc')
-          newOrders.push({ ...doc.data(), id: doc.id });
+        // listening all changes for queryOrders
+        const unsuscribe = onSnapshot(queryOrders, (snapshot) => {
+          let newOrders = [];
+          snapshot.forEach((doc) => {
+            newOrders.push({ ...doc.data(), id: doc.id });
+          });
+          setListOrders(newOrders);
         });
-        setListOrders(newOrders);
-      });
 
-      return () => unsuscribe();
+        return () => unsuscribe();
+      }
     }
     // eslint-disable-next-line
   }, [currentUser]);
 
-  // console.log(orderedTime)
-
+  // Food ordered history value
   const contentFoodList = useMemo(() => {
-    console.log(listOrders, 'listOrders')
     return listOrders ? listOrders.map(({ address, totalOrderSum, createdAt, foodItemList, id }) => {
       let date = '';
       let month = '';
@@ -111,14 +121,17 @@ const UserCabinet = (props) => {
       });
 
       return (
-        <div className="orders-list__item" key={id}>
+        <div 
+          className="orders-list__item" 
+          key={id}
+        >
           <div className="orders-list__details">
             <div className="orders-list__address">{address ? address : 'Пашутинська 61/84. Кропивницький'}</div>
             <div className="orders-list__date">
               {
-                createdAt ? 
-                `${date.length > 2 ? date.slice(1) : date}/${month}/${year}` :
-                ''
+                createdAt ?
+                  `${date.length > 2 ? date.slice(1) : date}/${month}/${year}` :
+                  ''
               }
             </div>
             <div className="orders-list__order-sum">{`${totalOrderSum} грн`}</div>
@@ -131,14 +144,15 @@ const UserCabinet = (props) => {
     }) : "Замовлення відсутні";
   }, [listOrders]);
 
-  console.log(contentFoodList)
-
   const showWindow = activeLogRegWindow ? 'user-cabinet_active' : '';
 
   return (
     <div className={`user-cabinet ${showWindow}`}>
       <div className="user-cabinet__overlay" onClick={toggleLogRegWindActive}>
-        <div className="user-cabinet__wrapper">
+        <div className={`form__success${successWindow ? ' form__success_active' : ''}`}>
+          Ви вийшли з власного кабінету!
+        </div>
+        <div className={`user-cabinet__wrapper${successWindow ? ' user-cabinet__wrapper_hide' : ''}`}>
           <div className="user-cabinet__icon-close">
             <i className='bx bx-x'></i>
           </div>
@@ -146,23 +160,14 @@ const UserCabinet = (props) => {
           <div className="user-cabinet__title">Ваш кабінет</div>
 
           <div className="user-cabinet__body">
-            {
-              currentUserName ? (
-                <div className="user-cabinet__user-name">
-                  <span>Ваше ім'я</span>
-                  <span>{currentUserName}</span>
-                </div>
-              ) : 'Відсутня інформація'
-            }
-            {
-              currentUserName ? (
-                <div className="user-cabinet__email">
-                  <span>E-mail</span>
-                  <span>{currentUser.email}</span>
-                </div>
-              ) : 'Відсутня інформація'
-            }
-
+            <div className="user-cabinet__user-name">
+              <span>Ваше ім'я</span>
+              <span>{currentUserName ? currentUserName : 'unkown'}</span>
+            </div>
+            <div className="user-cabinet__email">
+              <span>E-mail</span>
+              <span>{currentUser.email ? currentUser.email : 'unkown'}</span>
+            </div>
           </div>
 
           <button className='user-cabinet__logout' onClick={signUserOut}>Закінчити сесію</button>
@@ -173,29 +178,6 @@ const UserCabinet = (props) => {
               contentFoodList.length === 0 ?
                 <p className="orders-list__empty">Відсутня інформація</p> :
                 contentFoodList}
-            {/* <div className="orders-list__item">
-              <div className="orders-list__details">
-                <div className="orders-list__address">вул. Велика Перспективна 19, кв. 1 м. Кропивницький</div>
-                <div className="orders-list__date">13/01/2000</div>
-                <div className="orders-list__order-sum">1000 грн</div>
-              </div>
-              <div className="orders-list__food-list">
-                <div className="orders-list__food-item">
-                  <div className="orders-list__food-img">
-                    <img src={img} alt="" />
-                  </div>
-                  <div className="orders-list__food-name">Гавайська хвиля (4 м'яса)</div>
-                  <div className="orders-list__food-number">
-                    <span>1x</span>
-                    <span>26</span>
-                  </div>
-                  <div className="orders-list__sum">
-                    <div className="orders-list__for-one-item">180 грн</div>
-                    <div className="orders-list__total-sum">3600 грн</div>
-                  </div>
-                </div>
-              </div>
-            </div> */}
           </div>
         </div>
       </div>
