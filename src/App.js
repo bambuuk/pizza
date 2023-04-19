53b76cbae32ff57b49d70ebded36f601c0606808
Ivan <vanyok.korobka@ukr.net>
2023-04-19 14:34:33 +0300

    added info for rendering page on gh-pages

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import './style/style.scss';
 
 function App() {
   return (
-    <BrowserRouter>
+    <BrowserRouter basename="/pizza">
       <Suspense>
         <AnimatedRoutes />
       </Suspense>
